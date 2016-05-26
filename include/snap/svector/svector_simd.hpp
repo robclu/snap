ddb9dbb9d4e5c250c603fcba45e974218a906072
robjclucas@gmail.com <robclu1818@gmail.com>
2016-05-26 09:25:58 +0200

    added assembler cmake option

--- include/snap/svector/svector_simd.hpp
+++ include/snap/svector/svector_simd.hpp
@@ -1,4 +1,23 @@
-#include "svector_general.hpp"
+//---- snap/svector/svector_sse.hpp ----------------------- -*- C++ -*- ----//
+//
+//                                 Snap
+//                          
+//                      Copyright (c) 2016 Rob Clucas        
+//                    Distributed under the MIT License
+//                (See accompanying file LICENSE or copy at
+//                   https://opensource.org/licenses/MIT)
+//
+// ========================================================================= //
+//
+/// \file  svector_sse.hpp
+/// \brief Defiition of svector class SSE implementation.
+//
+//---------------------------------------------------------------------------//
+
+#ifndef SNAP_SVECTOR_SVECTOR_SSE_HPP
+#define SNAP_SVECTOR_SVECTOR_SSE_HPP
+
+#include "snap/config/simd_instruction_detect.h"
 
 namespace snap {
 
@@ -17,9 +36,21 @@ SNAP_ALIGN class svector {
   svector() {}
 
   /// Constructor to convert from sse intrinsics.
-  svector(const data_type& other_data) {
-    data = other_data;
-  }
+  svector(const data_type& other_data);
+
+  /// Constructor to broadcast a single value.
+  /// \param[in] val The value to boradcast.
+  
 };
 
+// ---- Implementation ----------------------------------------------------- //
+
+template <typename T, uint8_t W>
+svector<T, W>::svector(const svector::data_type& other_data) {
+  data = other_data;
+}
+
 } // namespace snap
+
+#endif // SNAP_SVECTOR_SVECTOR_SSE_HPP
+
